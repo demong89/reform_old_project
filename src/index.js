15ef39585b400308e9ce559672f503fd4719aa67
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-10-11 20:26:18 +0800

    feat: css 处理

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import './css/public.css'
+import './css/index.css'
+
+
 console.log('改造');
 console.log('没');
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,37 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode:"development",
-  entry:'./src/index.js',
-  output:{
-    filename:'bundle.js',
-    path:path.resolve(__dirname,'./dist')
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
-  plugins:[
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: "images/[name].[hash:6][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
     new HTMLWebpackPlugin({
-      filename:'index.html',
-      template:'./src/index.html'
-    })
-  ]
-}
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
