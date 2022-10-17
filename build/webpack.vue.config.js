9e25e377b968ea337f43ab25c0a25fbda719f566
guoxingwang <wb-gxw02154864@alibaba-inc.com>
2022-10-17 19:57:51 +0800

    feat: 配置Vue 环境   有坑  vue-loader:部分字段没有容错处理

---
+++ build/webpack.vue.config.js
@@ -0,0 +1,119 @@
+const path  = require("path")
+const webpack = require("webpack");
+
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyjsPlugin = require("uglifyjs-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+console.log(22222222222222);
+const VueLoaderPlugin = require("vue-loader/dist/plugin").default
+
+
+
+
+console.log('[VueLoaderPlugin:]',VueLoaderPlugin);
+
+
+const config = {
+  mode:'development',
+  entry:{
+    index:path.resolve(__dirname,'../src/main.js')
+  },
+  output:{
+    filename:'js/[name].js',
+    path:path.resolve(__dirname,'../dist')
+  },
+  devServer:{
+    static:{
+      directory:path.resolve(__dirname,"../dist")
+    },
+    compress:true,
+    port:9000,
+    hot:true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024,
+          },
+        },
+        generator: {
+          filename: "img/[name].[hash:6][ext]",
+        },
+      },
+      {
+        test:/\.ejs$/,
+        loader:'ejs-loader',
+        options:{
+          esModule:false
+        }
+      },
+      {
+        test:/\.vue$/,
+        loader:'vue-loader'
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      chunks: ["index"],
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./../src/img"),
+          to: path.resolve(__dirname, "./../dist/img"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "css/[name].chunk.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyjsPlugin({ /*sourceMap: true*/ }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: "all",
+      minSize: 100 * 1024,
+      name: "common222",
+      cacheGroups: {
+        jquery: {
+          name: "juery",
+          test: /jquery/, // 匹配名字
+          chunks: "all",
+        },
+
+        "loadash-es": {
+          name: "loadash-es",
+          test: /loadash-es/, // 匹配名字
+          chunks: "all",
+        },
+      },
+    },
+  },
+
+}
+module.exports = config
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="id">{{message}}</div>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      message:'Vue项目'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
